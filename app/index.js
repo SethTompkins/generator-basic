3a6904bf77af3386229ae2293c336f5812958113
SethTompkins <tompkins.seth01@gmail.com>
2014-07-07 01:35:48 -0400

    basic functionality added, issue on windows systems with permissions

--- app/index.js
+++ app/index.js
@@ -2,53 +2,109 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
-var chalk = require('chalk');
+var exec = require('child_process').exec;
 
 
-var BasicGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+var BasicGenerator = module.exports = function BasicGenerator(args, options, config) {
+  yeoman.generators.NamedBase.apply(this, arguments);
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
+  this.on('end', function () {
+    this.installDependencies({
+      skipInstall: options['skip-install'],
+      callback: function() {
+        console.log("\n\nAll dependencies have been installed! Running Grunt...\n\n");
+        exec("grunt");
+        console.log("All done!");
       }
     });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+
+  this.today = new Date();
+  this.fullDate = this.today.getFullYear();
+  this.fullDate += '-' + (this.today.getUTCMonth() + 1);
+  this.fullDate += '-' + this.today.getDate();
+};
+
+util.inherits(BasicGenerator, yeoman.generators.Base);
+
+BasicGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // Have Yeoman greet the user.
+  console.log(this.yeoman);
+  console.log("I just have a few questions to ask...\n");
+
+  var prompts = [{
+    type: 'input',
+    name: 'authorName',
+    message: 'What is your name?'
   },
-
-  askFor: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous Basic generator!'));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
+  {
+    type: 'input',
+    name: 'authorEmail',
+    message: 'What is your email address?'
   },
+  {
+    type: 'input',
+    name: 'jQueryVer',
+    message: "Which version of jQuery would you like? Use 'latest' for the most recent.",
+    default: '1.4.2'
+  }];
 
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
+  this.prompt(prompts, function (props) {
+    this.authorName = props.authorName;
+    this.authorEmail = props.authorEmail;
+    this.jQueryVer = props.jQueryVer;
 
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
+    cb();
+  }.bind(this));
+};
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  }
-});
+BasicGenerator.prototype.folders = function folders() {
+  this.mkdir('dev');
+  this.mkdir('dev/css');
+  this.mkdir('dev/scss');
+  this.mkdir('dev/scss/components');
+  this.mkdir('dev/scss/modules');
+  this.mkdir('dev/scss/page');
+  this.mkdir('dev/scss/vendor');
+  this.mkdir('dev/img');
+  this.mkdir('dev/js');
+  this.mkdir('dev/components');
+  this.mkdir('build');
+};
 
-module.exports = BasicGenerator;
+BasicGenerator.prototype.projectFiles = function projectFiles() {
+  // Git
+  this.copy('_.gitignore', '.gitignore');
+
+  // EditorConfig
+  this.copy('_.editorconfig', '.editorconfig');
+
+  // Node
+  this.template('_package.json', 'package.json');
+
+  // Grunt
+  this.copy('_gruntfile.js', 'gruntfile.js');
+
+  // Bower
+  this.template('_.bowerrc', '.bowerrc');
+  this.template('_bower.json', 'bower.json');
+
+  // Project
+  this.template('_dev/_index.html', 'dev/index.html');
+  this.copy('_dev/_scss/_style.scss', 'dev/scss/style.scss');
+  this.copy('_dev/_scss/_modules/_modules.scss', 'dev/scss/modules/modules.scss');
+  this.copy('_dev/_scss/_modules/_functions.scss', 'dev/scss/modules/functions.scss');
+  this.copy('_dev/_scss/_modules/_mixins.scss', 'dev/scss/modules/mixins.scss');
+  this.copy('_dev/_scss/_components/_components.scss', 'dev/scss/components/components.scss');
+  this.copy('_dev/_scss/_components/_layout.scss', 'dev/scss/components/layout.scss');
+  this.copy('_dev/_scss/_components/_typography.scss', 'dev/scss/components/typography.scss');
+  this.copy('_dev/_scss/_components/_variables.scss', 'dev/scss/components/variables.scss');
+  this.copy('_dev/_scss/_page/_page.scss', 'dev/scss/page/page.scss');
+  this.copy('_dev/_scss/_page/_index.scss', 'dev/scss/page/index.scss');
+  this.copy('_dev/_scss/_vendor/_vendor.scss', 'dev/scss/vendor/vendor.scss');
+  this.copy('_dev/_scss/_vendor/_reset.scss', 'dev/scss/vendor/reset.scss');
+};
---
+++ app/templates/_gruntfile.js
@@ -0,0 +1,203 @@
+module.exports = function(grunt) {
+    // Make project configuration changes here
+    var projectConfig = {
+        banner:
+            '/*!\n' +
+            ' * Title:    <%= pkg.name %>\n' +
+            ' * Author:   <%= pkg.author.name %>\n' +
+            ' *           <%= pkg.author.email %>\n' +
+            ' * Creation: <%= pkg.creation %>\n' +
+            ' */',
+        browsers: 'last 4 versions',
+        dev: {
+            dir: 'dev',
+            sass: 'dev/scss',
+            css: 'dev/css',
+            images: 'dev/img',
+            javascript: 'dev/js',
+            components: 'dev/components'
+        },
+        build: {
+            dir: 'build',
+            sass: 'build/scss',
+            css: 'build/css',
+            images: 'build/img',
+            javascript: 'build/js',
+            components: 'build/components'
+        },
+        server_port: 9000,
+        server_base: 'dev'
+    }
+
+    grunt.initConfig({
+        // Task configuration
+        pkg: grunt.file.readJSON('package.json'),
+        projectConfig: projectConfig,
+        sass: {
+            dev: {
+                options: {
+                    sourcemap: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.dev.sass %>',
+                    src: ['*.scss'],
+                    dest: '<%= projectConfig.dev.css %>',
+                    ext: '.css'
+                }]
+            },
+            build: {
+                options: {
+                    banner: '<%= projectConfig.banner %>',
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.build.sass %>',
+                    src: ['*.scss'],
+                    dest: '<%= projectConfig.build.css %>',
+                    ext: '.css'
+                }]
+            }
+        },
+        autoprefixer: {
+            dev: {
+                options: {
+                    browsers: ['<%= projectConfig.browsers %>']
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.dev.css %>',
+                    src: ['*.css'],
+                    dest: '<%= projectConfig.dev.css %>',
+                    ext: '.css'
+                }]
+            },
+            build: {
+                options: {
+                    browsers: ['<%= projectConfig.browsers %>']
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.build.css %>',
+                    src: ['*.css'],
+                    dest: '<%= projectConfig.build.css %>',
+                    ext: '.css'
+                }]
+            }
+        },
+        csscomb: {
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.dev.css %>',
+                    src: ['*.css'],
+                    dest: '<%= projectConfig.dev.css %>',
+                    ext: '.css'
+                }]
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.build.css %>',
+                    src: ['*.css'],
+                    dest: '<%= projectConfig.build.css %>',
+                    ext: '.css'
+                }]
+            }
+        },
+        imagemin: {
+            build: {
+                options: {
+                    optimizationLevel: 1
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.build.images %>',
+                    src: '*.{png,jpg,gif}',
+                    dest: '<%= projectConfig.build.images %>'
+                }]
+            }
+        },
+        clean: ['<%= projectConfig.build.dir %>'],
+        copy: {
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.dev.dir %>',
+                    src: ['**'],
+                    dest: '<%= projectConfig.build.dir %>'
+                }]
+            }
+        },
+        compress: {
+            build: {
+                options: {
+                    archive: '<%= projectConfig.build.images %>/images.zip',
+                    mode: 'zip'
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= projectConfig.build.images %>',
+                    src: ['*.{jpg,png,gif}']
+                }]
+            }
+        },
+        connect: {
+            options: {
+                port: '<%= projectConfig.server_port %>',
+                base: '<%= projectConfig.server_base %>',
+                livereload: true
+            },
+            open: true
+        },
+        watch: {
+            html: {
+                options: { livereload: true },
+                files: ['<%= projectConfig.dev.dir %>/**.html']
+            },
+            sass: {
+                files: ['<%= projectConfig.dev.sass %>/**'],
+                tasks: ['sass:dev', 'csscomb:dev', 'autoprefixer:dev']
+            },
+            css: {
+                options: { livereload: true },
+                files: ['<%= projectConfig.dev.css %>/**.css']
+            },
+            images: {
+                options: { livereload: true },
+                files: ['<%= projectConfig.dev.images %>/*']
+            },
+            components: {
+                files: ['<%= projectConfig.dev.components %>/**']
+            },
+            resources: {
+                files: ['<%= projectConfig.dev.resources %>/**']
+            }
+        }
+    });
+
+    // Automagically load all Grunt NPM tasks
+    require("load-grunt-tasks")(grunt);
+
+    // Register task groups
+    grunt.registerTask('default', [
+        'sass:dev',
+        'csscomb:dev',
+        'autoprefixer:dev'
+    ]);
+    grunt.registerTask('server', [
+        'connect',
+        'watch'
+    ]);
+    grunt.registerTask('build', [
+        'clean',
+        'copy:build',
+        'sass:build',
+        'csscomb:build',
+        'autoprefixer:build',
+        'imagemin',
+        'compress'
+    ]);
+
+};
